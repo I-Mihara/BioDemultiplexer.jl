84749470598713f0a408585dda088055e3deaab1
I.Mihara <issei.mihara@xforestx.com>
2023-12-12 10:48:37 +0000

    Implemented a new scoring and error rate system

--- src/demultiplexer.jl
+++ src/demultiplexer.jl
@@ -1,171 +1,167 @@
 @everywhere begin
-   """
-   semiglobal_alignment is a function that aligns two strings.
-   The first string is a query string and the second string is a sequence string.
-   The function returns the score of the alignment and the error rate of the alignment.
-   """
-   #search a string from b string
-   function semiglobal_alignment(query::String, seq::String; m::Int=Base.length(query), n::Int=Base.length(seq), match::Int=0, mismatch::Int=-1, indel::Int=-1)
-      if m == 0 || n == 0
-         return 0
-      end
-      DP = [indel * i for i in 1:m]
-      # run dynamic programming column by column (except the last column)
-      for j in 1:n
-         previous_score = 0
-         for i in 1:m
-             insertion_score = DP[i] + (i == m ? 0 : indel)#→
-             deletion_score = previous_score + indel#↓
-             substitution_score = (i == 1 ? 0 : DP[i-1]) + (query[i] == seq[j] ? match : mismatch)#↘︎
-            if i != 1
-               DP[i-1] = previous_score
-            end
-             previous_score = max(insertion_score, deletion_score, substitution_score)
-         end
-         DP[m] = previous_score
-      end
-      return 1+DP[m]/m
-   end
-   
-   """
-   classify_sequence is a function that classifies a query string into a sequence string.
-   The function returns the number of the sequence string and whether the query string is assigned to multiple sequence strings.
-   """
-   function classify_sequence(seq::String, bc_df::DataFrame,match::Int=0,mismatch::Int=-1,indel::Int=-2,max_error_rate::Float64=0.22)
-      maximum_score = -Inf
-      maximum_score_number = 0
-      multi_assignment = false
-      lenseq=length(seq)
-      for (i, row) in enumerate(eachrow(bc_df))
-         score, errorrate = semiglobal_alignment(row.Full_seq, seq, match=match, mismatch=mismatch, indel=indel, m=length(row.Full_seq), n=lenseq)
-         if errorrate < max_error_rate
-            if maximum_score < score
-               multi_assignment = false
-               maximum_score = score
-               maximum_score_number = i
-            elseif maximum_score == score
-               multi_assignment = true
-            end
-         end
-      end
-      return maximum_score_number, multi_assignment
-   end
-   """
-   determine_filemname is a function that determines the file name of the output file.
-   """
-   function determine_filemname(seq::String, bc_df::DataFrame; match::Int=0, mismatch::Int=-1, indel=-2, max_error_rate=0.22)
-      class_barcode, multi_assignment = classify_sequence(seq, bc_df, match, mismatch, indel, max_error_rate)
-      if multi_assignment
-         return "/multi_assignment.fastq"
-      elseif class_barcode == 0
-         return "/unknown.fastq"
-      else
-         return "/" * string(bc_df.ID[class_barcode]) * ".fastq"
-      end
-   end
-      
+	"""
+	semiglobal_alignment is a function that aligns two strings.
+	The first string is a query string and the second string is a sequence string.
+	The function returns the score of the alignment and the error rate of the alignment.
+	"""
+	#search a string from b string
+	function semiglobal_alignment(query::String, seq::String; m::Int = Base.length(query), n::Int = Base.length(seq), match::Int = 0, mismatch::Int = -1, indel::Int = -1)
+		if m == 0 || n == 0
+			return 0
+		end
+		DP = [indel * i for i in 1:m]
+		# run dynamic programming column by column (except the last column)
+		for j in 1:n
+			previous_score = 0
+			for i in 1:m
+				insertion_score = DP[i] + (i == m ? 0 : indel)#→
+				deletion_score = previous_score + indel#↓
+				substitution_score = (i == 1 ? 0 : DP[i-1]) + (query[i] == seq[j] ? match : mismatch)#↘︎
+				if i != 1
+					DP[i-1] = previous_score
+				end
+				previous_score = max(insertion_score, deletion_score, substitution_score)
+			end
+			DP[m] = previous_score
+		end
+		return 1 + DP[m] / m
+	end
 
-   """
-   write_fastq_entry is a function that writes a fastq entry to a file.
-   """  
-   function write_fastq_entry(filepath, header, seq, plus, quality)
-      open(filepath, "a") do outputfile
-         write(outputfile, header * "\n" * seq * "\n" * plus * "\n" * quality * "\n")
-      end
-   end
+	"""
+	classify_sequence is a function that classifies a query string into a sequence string.
+	The function returns the number of the sequence string and whether the query string is assigned to multiple sequence strings.
+	"""
+	function classify_sequence(seq::String, bc_df::DataFrame, max_error_rate::Float64)
+		max_score = -1
+		sub_max_score = -1
+		max_score_bc = 0
+		lenseq = length(seq)
+		for (i, row) in enumerate(eachrow(bc_df))
+			similarity_score = semiglobal_alignment(row.Full_seq, seq, n = lenseq)
+			if similarity_score > 1 - max_error_rate
+				if similarity_score > max_score
+					sub_max_score = max_score
+					max_score = similarity_score
+					max_score_bc = i
+				elseif similarity_score > sub_max_score
+					sub_max_score = similarity_score
+				end
+			end
+		end
+		delta = max_score - sub_max_score
+		return max_score_bc, delta
+	end
+	"""
+	determine_filemname is a function that determines the file name of the output file.
+	"""
+	function determine_filemname(seq::String, bc_df::DataFrame, max_error_rate::Float64, min_delta::Float64)
+		max_score_bc, delta = classify_sequence(seq, bc_df, max_error_rate)
+		if max_score_bc == 0
+			return "/unknown.fastq"
+		elseif delta < min_delta
+			return "/ambiguous_classification.fastq"
+		else
+			return "/" * string(bc_df.ID[max_score_bc]) * ".fastq"
+		end
+	end
 
-   """
-   process_fastq_file is a function that processes a fastq file.
-   """
-   function process_fastq_file(fastq_R1::String, fastq_R2::String, bc_df::DataFrame, output_dir::String; match=0, mismatch=-1, indel=-2, classify_r1=false, classify_r2=true)
-      if classify_r2
-         if classify_r1
-            mkdir(output_dir * "/R1")
-            mkdir(output_dir * "/R2")
-            open(fastq_R1, "r") do primary_file
-               open(fastq_R2, "r") do secondary_file
-                  header, seq, plus, quality_score = "", "", "", ""
-                  header2, seq2, plus2, quality_score2 = "", "", "", ""
-                  mode = "header"
-                  filename = ""
-                  for line1 in eachline(primary_file)
-                     line2 = readline(secondary_file)
-                     if line1[1] == '@' && mode == "header"
-                        header = line
-                        header2 = line2
-                        mode = "seq"
-                     elseif mode == "seq"
-                        seq = line
-                        seq2 = line2
-                        mode = "plus"
-                     elseif mode == "plus"
-                        plus = line
-                        plus2 = line2
-                        mode = "quality_score"
-                     elseif mode == "quality_score"
-                        quality_score = line
-                        quality_score2 = line2
-                        filename = determine_filemname(seq, bc_df, match=match, mismatch=mismatch, indel=indel)
-                        write_fastq_entry(output_dir * "/R1" * filename, header, seq, plus, quality_score)
-                        write_fastq_entry(output_dir * "/R2" * filename, header2, seq2, plus2, quality_score2)
-                        mode = "header"
-                     end
-                  end
-               end
-            end
-         else
-            open(fastq_R1, "r") do primary_file
-               open(fastq_R2, "r") do secondary_file
-                  header2, seq2, plus2, quality_score2 = "", "", "", ""
-                  mode = "header"
-                  filename = ""
-                  count = 0
 
-                  for line1 in eachline(primary_file)
-                     line2 = readline(secondary_file)
-                        if line1[1] == '@' && mode == "header"
-                           header2 = line2
-                           mode = "seq"
-                        elseif mode == "seq"
-                           filename = determine_filemname(line1, bc_df, match=match, mismatch=mismatch, indel=indel)
-                           seq2 = line2
-                           mode = "plus"
-                        elseif mode == "plus"
-                           plus2 = line2
-                           mode = "quality_score"
-                        elseif mode == "quality_score"
-                           quality_score2 = line2
-                           write_fastq_entry(output_dir * filename, header2, seq2, plus2, quality_score2)
-                           mode = "header"
-                        end
-                     count += 1
-                  end
-               end
-            end
-         end
-      elseif classify_r1
-         open(fastq_R1, "r") do file
-            header, seq, plus, quality_score = "", "", "", ""
-            mode = "header"
-            filename = ""
-            for line in eachline(file)
-               if line[1] == '@' && mode == "header"
-                  header = line
-                  mode = "seq"
-               elseif mode == "seq"
-                  seq = line
-                  mode = "plus"
-               elseif mode == "plus"
-                  plus = line
-                  mode = "quality_score"
-               elseif mode == "quality_score"
-                  quality_score = line
-                  filename = determine_filemname(seq, bc_df, match=match, mismatch=mismatch, indel=indel)
-                  write_fastq_entry(output_dir * filename, header, seq, plus, quality_score)
-                  mode = "header"
-               end
-            end
-         end
-      end
-   end
+	"""
+	write_fastq_entry is a function that writes a fastq entry to a file.
+	"""
+	function write_fastq_entry(filepath, header, seq, plus, quality)
+		open(filepath, "a") do outputfile
+			write(outputfile, header * "\n" * seq * "\n" * plus * "\n" * quality * "\n")
+		end
+	end
+
+	"""
+	process_fastq_file is a function that processes a fastq file.
+	"""
+	function process_fastq_file(fastq_R1::String, fastq_R2::String, bc_df::DataFrame, output_dir::String, max_error_rate::Float64, min_delta::Float64; classify = "R2")
+		if classify == "both"
+			mkdir(output_dir * "/R1")
+			mkdir(output_dir * "/R2")
+			open(fastq_R1, "r") do primary_file
+				open(fastq_R2, "r") do secondary_file
+					header, seq, plus, quality_score = "", "", "", ""
+					header2, seq2, plus2, quality_score2 = "", "", "", ""
+					mode = "header"
+					filename = ""
+					for line1 in eachline(primary_file)
+						line2 = readline(secondary_file)
+						if line1[1] == '@' && mode == "header"
+							header = line
+							header2 = line2
+							mode = "seq"
+						elseif mode == "seq"
+							seq = line
+							seq2 = line2
+							mode = "plus"
+						elseif mode == "plus"
+							plus = line
+							plus2 = line2
+							mode = "quality_score"
+						elseif mode == "quality_score"
+							quality_score = line
+							quality_score2 = line2
+							filename = determine_filemname(seq, bc_df, match = match, max_error_rate, min_delta)
+							write_fastq_entry(output_dir * "/R1" * filename, header, seq, plus, quality_score)
+							write_fastq_entry(output_dir * "/R2" * filename, header2, seq2, plus2, quality_score2)
+							mode = "header"
+						end
+					end
+				end
+			end
+		elseif classify == "R2"
+			open(fastq_R1, "r") do primary_file
+				open(fastq_R2, "r") do secondary_file
+					header2, seq2, plus2, quality_score2 = "", "", "", ""
+					mode = "header"
+					filename = ""
+					for line1 in eachline(primary_file)
+						line2 = readline(secondary_file)
+						if line1[1] == '@' && mode == "header"
+							header2 = line2
+							mode = "seq"
+						elseif mode == "seq"
+							filename = determine_filemname(line1, bc_df, max_error_rate, min_delta)
+							seq2 = line2
+							mode = "plus"
+						elseif mode == "plus"
+							plus2 = line2
+							mode = "quality_score"
+						elseif mode == "quality_score"
+							quality_score2 = line2
+							write_fastq_entry(output_dir * filename, header2, seq2, plus2, quality_score2)
+							mode = "header"
+						end
+					end
+				end
+			end
+		elseif classify == "R1"
+			open(fastq_R1, "r") do file
+				header, seq, plus, quality_score = "", "", "", ""
+				mode = "header"
+				filename = ""
+				for line in eachline(file)
+					if line[1] == '@' && mode == "header"
+						header = line
+						mode = "seq"
+					elseif mode == "seq"
+						seq = line
+						mode = "plus"
+					elseif mode == "plus"
+						plus = line
+						mode = "quality_score"
+					elseif mode == "quality_score"
+						quality_score = line
+						filename = determine_filemname(seq, bc_df, max_error_rate, min_delta)
+						write_fastq_entry(output_dir * filename, header, seq, plus, quality_score)
+						mode = "header"
+					end
+				end
+			end
+		end
+	end
 end
--- src/main.jl
+++ src/main.jl
@@ -1,78 +1,79 @@
 @everywhere begin
-   function preprocess_bc_file(bc_file_path::String; rev=true)
-      bc_df = CSV.read(bc_file_path, DataFrame, delim="\t")
-      for i in 1:nrow(bc_df)
-          prefix_region = 1:findfirst('B', bc_df.Full_annotation[i])-1
-          suffix_region = findlast('B', bc_df.Full_annotation[i])+1:length(bc_df.Full_annotation[i])
-          prefix = SubString(bc_df.Full_seq[i], prefix_region)
-          suffix = SubString(bc_df.Full_seq[i], suffix_region)
-          bc_df.Full_seq[i] = replace(bc_df.Full_seq[i], prefix => "")
-          bc_df.Full_seq[i] = replace(bc_df.Full_seq[i], suffix => "")
-      end
-      bc_df.Full_seq = uppercase.(bc_df.Full_seq)
-      bc_df.Full_seq = replace.(bc_df.Full_seq, "U" => "T")
-      if rev == true
-          bc_df.Full_seq = reverse.(bc_df.Full_seq)
-          bc_df.Full_seq = replace.(bc_df.Full_seq, "A" => "T", "T" => "A", "G" => "C", "C" => "G")
-      end
-      return bc_df
-  end
-  
-  function execute_demultiplexing(file_R1,file_R2,bc_file,output_dir)
-      if isdir(output_dir)
-          rm(output_dir, recursive=true)
-      end
-      mkdir(output_dir)
-      workers = nworkers()
-      bc_df = preprocess_bc_file(bc_file)
-      if workers == 1
-          process_fastq_file(file_R1, file_R2, bc_df, output_dir)
-      else
-           divide_fastq(file_R1,file_R2,output_dir, workers)
-           pmap(x -> mlt_demltplex(x, bc_df, output_dir), 1:workers)
-           f = []
-           for (root, dirs ,files) in walkdir(output_dir)
-                 for file in files
-                    push!(f, joinpath(root, file))
-                 end
-           end   
-           dr_unknown = filter(x -> occursin(r"thread.*/unknown.fastq", x), f)
-           dr_multi_assignment = filter(x -> occursin(r"thread.*/multi_assignment.fastq", x), f)
-           run(pipeline(`cat $dr_unknown` ,stdout= "$output_dir/unknown.fastq"))
-           if dr_multi_assignment != []
-              run(pipeline(`cat $dr_multi_assignment` ,stdout= "$output_dir/multi_assignment.fastq"))
-           end
-           for i in 1:nrow(bc_df)
-                 regex1 = r"thread.*/"* string(bc_df.ID[i]) * ".fastq"
-                 dr_matched = filter(x -> occursin(regex1, x), f)
-                 if dr_matched != []
-                    dr_matched = joinpath.(output_dir, dr_matched)
-                    run(pipeline(`cat $dr_matched`, stdout= "$output_dir/$(bc_df.ID[i]).fastq"))
-                 end    
-           end
-           rm(joinpath(output_dir, "divided_fastq"), recursive=true)
-           for i in 1:workers
-              rm(joinpath(output_dir, "thread"*string(i)), recursive=true)
-           end
-      end
-  end
-      
-  function divide_fastq(file_R1,file_R2,output_dir,workers)
-      div_dir = joinpath(output_dir, "divided_fastq")
-      mkdir(div_dir)
-      num_lines = countlines(file_R1)
-      num_reads = num_lines ÷ 4
-      reads_per_thread = cld(num_reads, workers)
-      lines_per_thread = reads_per_thread*4
-      run(`split -l $lines_per_thread -a 5 -d $file_R1 $div_dir/R1_ --additional-suffix=.fastq`)
-      run(`split -l $lines_per_thread -a 5 -d $file_R2 $div_dir/R2_ --additional-suffix=.fastq`)
-  end
-  
-  function mlt_demltplex(thread_num, bc_df, output_dir)
-      fastq_R1 = output_dir * "/divided_fastq" * "/R1_" * lpad((thread_num-1),5,"0") * ".fastq"
-      fastq_R2 = output_dir * "/divided_fastq" * "/R2_" * lpad((thread_num-1),5,"0") * ".fastq"
-      mkdir(output_dir * "/thread" * string(thread_num))
-      output_dir = output_dir * "/thread" * string(thread_num)
-      process_fastq_file(fastq_R1, fastq_R2, bc_df, output_dir)
-  end      
+	function preprocess_bc_file(bc_file_path::String; rev = true)
+		bc_df = CSV.read(bc_file_path, DataFrame, delim = "\t")
+		for i in 1:nrow(bc_df)
+			prefix_region = 1:findfirst('B', bc_df.Full_annotation[i])-1
+			suffix_region = findlast('B', bc_df.Full_annotation[i])+1:length(bc_df.Full_annotation[i])
+			prefix = SubString(bc_df.Full_seq[i], prefix_region)
+			suffix = SubString(bc_df.Full_seq[i], suffix_region)
+			bc_df.Full_seq[i] = replace(bc_df.Full_seq[i], prefix => "")
+			bc_df.Full_seq[i] = replace(bc_df.Full_seq[i], suffix => "")
+		end
+		bc_df.Full_seq = uppercase.(bc_df.Full_seq)
+		bc_df.Full_seq = replace.(bc_df.Full_seq, "U" => "T")
+		if rev == true
+			bc_df.Full_seq = reverse.(bc_df.Full_seq)
+			bc_df.Full_seq = replace.(bc_df.Full_seq, "A" => "T", "T" => "A", "G" => "C", "C" => "G")
+		end
+		return bc_df
+	end
+
+	function execute_demultiplexing(file_R1, file_R2, bc_file, output_dir; max_error_rate = 0.2, min_delta = 0.1, classify = "R2", bc_rev = true)
+		if isdir(output_dir)
+			error("Output directory already exists")
+		end
+		mkdir(output_dir)
+		workers = nworkers()
+		bc_df = preprocess_bc_file(bc_file)
+		if workers == 1
+			process_fastq_file(file_R1, file_R2, bc_df, output_dir, max_error_rate, min_delta, classify = classify)
+		else
+			divide_fastq(file_R1, file_R2, output_dir, workers)
+			pmap(x -> mlt_demltplex(x, bc_df, output_dir, max_error_rate, min_delta, classify = classify), 1:workers)
+			paths = []
+			for (root, dirs, files) in walkdir(output_dir)
+				for file in files
+					push!(paths, joinpath(root, file))
+				end
+			end
+			paths_unknown = filter(x -> occursin(r"thread.*/unknown.fastq", x), paths)
+			paths_ambiguous_classification = filter(x -> occursin(r"thread.*/ambiguous_classification.fastq", x), paths)
+			# merge files
+			run(pipeline(`cat $paths_unknown`, stdout = "$output_dir/unknown.fastq"))
+			if paths_ambiguous_classification != []
+				run(pipeline(`cat $paths_ambiguous_classification`, stdout = "$output_dir/ambiguous_classification.fastq"))
+			end
+			for i in 1:nrow(bc_df)
+				regex1 = r"thread.*/" * string(bc_df.ID[i]) * ".fastq"
+				paths_matched = filter(x -> occursin(regex1, x), paths)
+				if paths_matched != []
+					paths_matched = joinpath.(output_dir, paths_matched)
+					run(pipeline(`cat $paths_matched`, stdout = "$output_dir/$(bc_df.ID[i]).fastq"))
+				end
+			end
+			rm(joinpath(output_dir, "divided_fastq"), recursive = true)
+			for i in 1:workers
+				rm(joinpath(output_dir, "thread" * string(i)), recursive = true)
+			end
+		end
+	end
+
+	function divide_fastq(file_R1, file_R2, output_dir, workers)
+		div_dir = joinpath(output_dir, "divided_fastq")
+		mkdir(div_dir)
+		num_lines = countlines(file_R1)
+		num_reads = num_lines ÷ 4
+		reads_per_thread = cld(num_reads, workers)
+		lines_per_thread = reads_per_thread * 4
+		run(`split -l $lines_per_thread -a 5 -d $file_R1 $div_dir/R1_ --additional-suffix=.fastq`)
+		run(`split -l $lines_per_thread -a 5 -d $file_R2 $div_dir/R2_ --additional-suffix=.fastq`)
+	end
+
+	function mlt_demltplex(thread_num, bc_df, output_dir, max_error_rate, min_delta, classify)
+		fastq_R1 = output_dir * "/divided_fastq" * "/R1_" * lpad((thread_num - 1), 5, "0") * ".fastq"
+		fastq_R2 = output_dir * "/divided_fastq" * "/R2_" * lpad((thread_num - 1), 5, "0") * ".fastq"
+		mkdir(output_dir * "/thread" * string(thread_num))
+		output_dir = output_dir * "/thread" * string(thread_num)
+		process_fastq_file(fastq_R1, fastq_R2, bc_df, output_dir, max_error_rate, min_delta, classify = classify)
+	end
 end
